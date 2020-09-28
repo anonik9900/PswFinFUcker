1a0fb6afc92aca42d2daf5fbe5596a83ad35afd6
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2020-09-29 00:39:13 +0200

    dep installer

---
+++ need_this.sh
@@ -0,0 +1,14 @@
+echo "PswFinFUcker Dependencies installer"
+
+sudo apt update 
+sudo apt-get install python
+sudo apt-get install python2
+sudo apt update 
+sudo apt-get install python-pip
+sudo apt-get install curl
+sudo apt update 
+sudo curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
+sudo pip install pyfiglet
+sudo pip3 install pyfiglet
+sudo apt-get update
